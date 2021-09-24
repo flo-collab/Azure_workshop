b9966b375dbc1156e9dc9761f7539209873b070f
Flo <f.u.etudes@laposte.net>
2021-09-24 11:41:30 +0200

    lalala

---
+++ api/tasks-get/index.js
@@ -0,0 +1,34 @@
+tasks = [
+    {
+        id: 1,
+        label: "🍔 Eat",
+        status: ""
+    },
+    {
+        id: 2,
+        label: "🛏 Sleep",
+        status: ""
+    },
+    {
+        id: 3,
+        label: "</> Code",
+        status: ""
+    }
+];
+
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
+    return tasks
