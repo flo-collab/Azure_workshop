b347578afee7f1037dbc65111f3d5080ade890f2
Flo <f.u.etudes@laposte.net>
2021-09-24 13:45:32 +0200

    lalala

---
+++ api/HttpTrigger/__init__.py
@@ -0,0 +1,43 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+        
+tasks = [
+    {
+        id: 1,
+        label: "🍔 Eat",
+        status: ""
+    },
+    {
+        id: 2,
+        label: "🛏 Sleep",
+        status: ""
+    },
+    {
+        id: 3,
+        label: "</> Code",
+        status: ""
+    }
+];
